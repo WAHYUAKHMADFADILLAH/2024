1d99e39aa2de3166e96800f99ca1abcced644a72
wahyuakhmadfadillah <wahyufadil1140@gmail.com>
2024-04-23 20:03:51 +0700

    apsi jir

--- Python/Algoritma/fungsi dan modul/mat.py
+++ Python/Algoritma/fungsi dan modul/mat.py
@@ -3,14 +3,17 @@ def penjumlahan(a,b):
 def pengurangan(a,b):
   return a-b
 def perkalian(a,b):
-  return a*b
+    return a*b
+def pembagian(a,b):
+    return a/b
 def tampilan_main():
   print('pilihan')
   print('1.penjumlahan')
   print('2.pengurangan')
   print('3.perkalian')
-  pilihan = input('masukan pihan (1/2/3) : ')
-  if pilihan not in ('1','2','3'):
+  print('4.pembagian')
+  pilihan = input('masukan pihan (1/2/3/4) : ')
+  if pilihan not in ('1','2','3','4'):
     print('pilihan operator tidak valid')
   else:
    angka1 = int(input('masukan angka pertama : '))
@@ -22,3 +25,5 @@ def tampilan_main():
     print('hasil penjumlahan : ',pengurangan(angka1,angka2))
   elif pilihan == '3' :
     print('hasil penjumlahan : ',perkalian(angka1,angka2))
+  elif pilihan == '4':
+    print('hasil pembagian : ',pembagian(angka1,angka2))
