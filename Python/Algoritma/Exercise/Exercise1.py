88eabd7ed570accc22e8e7481a9b57b09d46bf57
wahyu akhmad fadillah <wahyufadil1140@gmail.com>
2024-05-05 16:37:58 +0700

    l

--- Python/Algoritma/Exercise/Exercise1.py
+++ Python/Algoritma/Exercise/Exercise1.py
@@ -1,11 +1,25 @@
-judul_buku = 'cara bangun pagi' #string
-tahun_terbit = 2024 #int
-harga = 2.000 #float
-penulis = 'putra nur izzatul' #string
-ketersediaan = True #boolean
-list = (judul_buku,tahun_terbit,harga,penulis,ketersediaan)
-print(list)
-print('judul_buku :',judul_buku)
-print(harga)
-judul_buku = input('masukan judul buku baru : ')
-print('judul_buku baru : ',judul_buku)
+def buku():
+    judul_buku = 'cara bangun pagi'
+    print('judul_buku =',judul_buku)
+    tahun_terbit = 2024
+    print('tahun terbit =',tahun_terbit)
+    harga = 20000
+    print('harga =',harga)
+    penulis = 'wahyu akmad fadillah'
+    print('penulis =',penulis)
+    # ketersediaan = True
+    # list = (judul_buku,tahun_terbit,harga,penulis,ketersediaan)
+    judul_baru = input('masukan judul buku baru : ')
+    print(f'judul buku baru : {judul_baru}')
+buku()
+# judul_buku = 'cara bangun pagi' #string
+# tahun_terbit = 2024 #int
+# harga = 2.000 #float
+# penulis = 'putra nur izzatul' #string
+# ketersediaan = True #boolean
+# list = (judul_buku,tahun_terbit,harga,penulis,ketersediaan)
+# print(list)
+# print('judul_buku :',judul_buku)
+# print(harga)
+# judul_buku = input('masukan judul buku baru : ')
+# print('judul_buku baru : ',judul_buku)
--- Python/Algoritma/Exercise/Exercise2.py
+++ Python/Algoritma/Exercise/Exercise2.py
@@ -1,8 +1,10 @@
-nama = input('masukan nama : ')
-jam_kerja = int(input('jam kerja   : '))
-tarif_perjam = 30000 
-jumlah_gaji = tarif_perjam * jam_kerja
-print('nama         :',nama)
-print('jam kerja    :',str(jam_kerja),'jam')
-print('tarif_perjam :',tarif_perjam)
-print('jumlah gaji  :',jumlah_gaji)
+def penghitung_jumlah_gaji():
+    nama = input('masukan nama : ')
+    jam_kerja = int(input('jam kerja (jam): '))
+    tarif_perjam = 30000 
+    jumlah_gaji = tarif_perjam * jam_kerja
+    print('nama         :',nama)
+    print('jam kerja    :',str(jam_kerja),'jam')
+    print('tarif_perjam :',tarif_perjam)
+    print('jumlah gaji  :',jumlah_gaji)
+penghitung_jumlah_gaji()
