386ff94cd01c0e3871357ee3d5b1b200c71c8e92
wahyu akhmad fadillah <wahyufadil1140@gmail.com>
2024-05-05 14:43:23 +0700

    ma

--- LoliProject/meh/main.py
+++ LoliProject/meh/main.py
@@ -1,23 +1,40 @@
-def nama():
-    nama = input('masukan nama : ')
-    return nama
+# def nama():
+#     nama = input('masukan nama : ')
+#     return nama
 
-def jam_kerja():
-    jam_kerja = int(input('jam kerja   : '))
-    return jam_kerja
+# def jam_kerja():
+#     jam_kerja = int(input('jam kerja   : '))
+#     return jam_kerja
 
-def tarif_perjam():
-    tarif_perjam = 30000
-    return tarif_perjam
+# def tarif_perjam():
+#     tarif_perjam = 30000
+#     return tarif_perjam
 
-def login_system():
-    nim = input('masukan NIM anda : ')
-    password = input('masukan password anda (NIM) : ')
+# def login_system():
+#     nim = input('masukan NIM anda : ')
+#     password = input('masukan password anda (NIM) : ')
 
-    if nim == '12345678' and password == '12345678':
-        print(f'Selamat datang, {nama()}!')
-    else:
-        print('Maaf, NIM atau password anda salah. Silakan coba lagi.')
-nim = ''
-password = ''
-login_system()
+#     if nim == '12345678' and password == '12345678':
+#         print(f'Selamat datang, {nama()}!')
+#     else:
+#         print('Maaf, NIM atau password anda salah. Silakan coba lagi.')
+# nim = ''
+# password = ''
+# login_system
+# def input_nama():
+#     while True:
+#         nama = input('Masukan nama = ')
+#         if not nama or not isinstance(nama,str):
+#             print('nama  tidak valid, Masukan Ulang!')
+#         else:
+#             print(f'Wow {nama}, keren sekali namamu!')
+#             return nama
+# input_nama()
+import random
+nomor = random.randint(0,2)
+tebak = int(input('masukan nomor tebakan anda! :'))
+if tebak == nomor:
+    print(f'selamat, tebakan anda: {tebak} Benar')
+else:
+    print(f'tebakan anda : {tebak} salah, yang benar {nomor}')
+# print(tebak)
