1104976c6a0ab355d747e6bdfde8e6fded3ed229
rusdi <wahyufadil1140@gmail.com>
2024-05-25 16:18:38 +0700

    commit

--- Algoritma/Exercise/Exercise1.py
+++ Algoritma/Exercise/Exercise1.py
@@ -3,23 +3,17 @@ def buku():
     print('judul_buku =',judul_buku)
     tahun_terbit = 2024
     print('tahun terbit =',tahun_terbit)
-    harga = 20000
+    harga = 20000 
     print('harga =',harga)
     penulis = 'wahyu akmad fadillah'
     print('penulis =',penulis)
-    # ketersediaan = True
+    # ketersediaan = True   
     # list = (judul_buku,tahun_terbit,harga,penulis,ketersediaan)
     judul_baru = input('masukan judul buku baru : ')
     print(f'judul buku baru : {judul_baru}')
 buku()
-# judul_buku = 'cara bangun pagi' #string
-# tahun_terbit = 2024 #int
-# harga = 2.000 #float
-# penulis = 'putra nur izzatul' #string
-# ketersediaan = True #boolean
-# list = (judul_buku,tahun_terbit,harga,penulis,ketersediaan)
-# print(list)
-# print('judul_buku :',judul_buku)
-# print(harga)
-# judul_buku = input('masukan judul buku baru : ')
-# print('judul_buku baru : ',judul_buku)
+
+
+
+print('===============')
+# 123456789023456783456789456789
